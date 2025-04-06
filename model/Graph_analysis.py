f0f3a3b851b36c5401ab67c14b9878cbe1124860
Will Colglazier <139183369+wcolglazier@users.noreply.github.com>
2025-04-06 17:56:38 -0600

    Update Graph_analysis.py

--- model/Graph_analysis.py
+++ model/Graph_analysis.py
@@ -7,61 +7,6 @@ import torch.nn.init as init
 from model import Time_analysis
 
 
-
-class ChebGraphConv(nn.Module):
-    def __init__(self, c_in, c_out, Ks, gso, bias):
-        super(ChebGraphConv, self).__init__()
-        self.c_in = c_in
-        self.c_out = c_out
-        self.Ks = Ks
-        self.gso = gso
-        self.weight = nn.Parameter(torch.FloatTensor(Ks, c_in, c_out))
-        if bias:
-            self.bias = nn.Parameter(torch.FloatTensor(c_out))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-        if self.bias is not None:
-            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-            init.uniform_(self.bias, -bound, bound)
-
-    def forward(self, x):
-        # bs, c_in, ts, n_vertex = x.shape
-        x = torch.permute(x, (0, 2, 3, 1))
-
-        if self.Ks - 1 < 0:
-            raise ValueError(
-                f'ERROR: the graph convolution kernel size Ks has to be a positive integer, but received {self.Ks}.')
-        elif self.Ks - 1 == 0:
-            x_0 = x
-            x_list = [x_0]
-        elif self.Ks - 1 == 1:
-            x_0 = x
-            x_1 = torch.einsum('hi,btij->bthj', self.gso, x)
-            x_list = [x_0, x_1]
-        elif self.Ks - 1 >= 2:
-            x_0 = x
-            x_1 = torch.einsum('hi,btij->bthj', self.gso, x)
-            x_list = [x_0, x_1]
-            for k in range(2, self.Ks):
-                x_list.append(torch.einsum('hi,btij->bthj', 2 * self.gso, x_list[k - 1]) - x_list[k - 2])
-
-        x = torch.stack(x_list, dim=2)
-
-        cheb_graph_conv = torch.einsum('btkhi,kij->bthj', x, self.weight)
-
-        if self.bias is not None:
-            cheb_graph_conv = torch.add(cheb_graph_conv, self.bias)
-        else:
-            cheb_graph_conv = cheb_graph_conv
-
-        return cheb_graph_conv
-
-
 class GraphConv(nn.Module):
     def __init__(self, c_in, c_out, gso, bias):
         super(GraphConv, self).__init__()
